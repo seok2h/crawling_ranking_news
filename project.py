8f62360efe47248a9cc2d4494116d4baffa2ab4b
Han Seungseok <cream5432111@gmail.com>
2024-02-23 20:50:13 +0900

    complete project

--- project.py
+++ project.py
@@ -5,39 +5,55 @@
     - 데이터프레임으로 만들어서 csv 저장하기
 """
 
-import pprint
+import datetime as dt
+# import pprint
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 # from selenium import webdriver
 
-URL = "https://news.naver.com/main/ranking/popularDay.naver"
+date = dt.datetime.now()
+company_list = ['한국경제', '매일경제', '서울경제']
 
-# browser = webdriver.Chrome()
-# browser.get(URL)
+data = pd.DataFrame(columns=company_list)
 
-# soup = BeautifulSoup(browser.page_source, "lxml")
+for x in range(7):
+    date_str = date.strftime("%Y%m%d")
+    url = f"https://news.naver.com/main/ranking/popularDay.naver?date={date_str}"
 
-res = requests.get(URL, timeout=30)
-res.raise_for_status()
-soup = BeautifulSoup(res.text, "lxml")
+    # browser = webdriver.Chrome()
+    # browser.get(URL)
+    # soup = BeautifulSoup(browser.page_source, "lxml")
 
-news = soup.find("div", attrs={"class": "_officeCard _officeCard0"})
-news_dict = {}
-news = news.find_all("div", attrs={"class": "rankingnews_box"})
+    res = requests.get(url, timeout=30)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, "lxml")
 
-for elem in news:
-    company_name = elem.find("strong", attrs={"class": "rankingnews_name"}).get_text()
-    titles = elem.find_all("a", attrs={"class": "list_title nclicks('RBP.rnknws')"})
-    title = [t.get_text() for t in titles]
-    news_dict[company_name] = title
+    news = soup.find_all("div", attrs={"class": "rankingnews_box"})
+    news_dict = {}
+    # news = news.find_all("div", attrs={"class": "rankingnews_box"})
 
-# df = pd.DataFrame.from_dict(data=news_dict, orient='columns')
+    # for elem in news:
+    #     company_name = elem.find("strong", attrs={"class": "rankingnews_name"}).get_text()
+    #     titles = elem.find_all("a", attrs={"class": "list_title nclicks('RBP.rnknws')"})
+    #     title = [t.get_text() for t in titles]
+    #     news_dict[company_name] = title
 
-pprint.pprint(news_dict)
+    # df = pd.DataFrame.from_dict(data=news_dict, orient='columns')
 
-df = pd.DataFrame.from_dict(data=news_dict, orient='columns')
+    for elem in news:
+        company_name = elem.find("strong", attrs={"class": "rankingnews_name"}).get_text()
+        if company_name in company_list:
+            # news_dict[company_name] = e
+            titles = elem.find_all("a", attrs={"class": "list_title nclicks('RBP.rnknws')"})
+            title_list = [title.get_text() for title in titles]
+            news_dict[company_name] = title_list
+            data.loc[date.strftime("%Y-%m-%d")] = pd.Series(data=news_dict, dtype=object)
 
-df.to_csv('data.csv', encoding='utf=-8')
+        else:
+            continue
 
-# input("종료하려면 Enter를 입력하세요")
+    date -= dt.timedelta(days=1)
+print(data.dtypes)
+data.to_csv('data.csv', encoding='utf-8')
+    # input("종료하려면 Enter를 입력하세요")
